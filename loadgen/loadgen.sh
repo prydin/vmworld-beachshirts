2d3120ca7b7f1b7bb5a7abbef6194940d8f5f526
prydin <prydin@vmware.com>
2019-08-01 08:37:32 -0400

    Added proxy.yml

--- loadgen/loadgen.sh
+++ loadgen/loadgen.sh
@@ -6,7 +6,7 @@ while [ true ]
 do
 	quantity=$((1 + RANDOM % 20))
 	style=$((1 + RANDOM % 20))
-	curl_cmd="curl -X POST http://$1:50050/shop/order -H 'Content-Type: application/json' -d '{\"styleName\": \"beachops$style\", \"quantity\": '$quantity'}'"
+	curl_cmd="curl -X POST http://$1:50050/api/shop/order -H 'Content-Type: application/json' -d '{\"styleName\": \"beachops$style\", \"quantity\": '$quantity'}'"
 	echo $curl_cmd
 	eval $curl_cmd
 	sleep $2
